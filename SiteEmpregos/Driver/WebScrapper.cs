7ab936ba1b3b25cc082ed614270a9fd5c2db0892
Lucas Dantas <lucas.dantas.nogueira@gmail.com>
2024-01-08 17:03:31 -0300

    Add project files.

---
+++ SiteEmpregos/Driver/WebScrapper.cs
@@ -0,0 +1,39 @@
+﻿using EasyAutomationFramework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using SiteEmpregos.Model;
+using System.Data;
+
+namespace SiteEmpregos.Driver
+{
+    public class WebScrapper : Web
+    {
+        public DataTable GetData(string link)
+        {
+            if (driver == null)
+                StartBrowser();
+
+            List<Item> items = new List<Item>();
+
+            Navigate(link);
+
+            var elements = GetValue(TypeElement.Xpath, "/html/body/form/div[3]/div/div[2]/article/div[3]")
+                            .element.FindElements(By.ClassName("descricao"));
+
+            foreach (var element in elements)
+            {
+                Item item = new Item();
+
+                item.Title = element.FindElement(By.TagName("h2")).Text;
+                item.Location = element.FindElement(By.ClassName("nome-empresa")).Text;
+                item.Description = element.FindElement(By.ClassName("resumo-vaga")).Text;
+                item.Company = element.FindElement(By.ClassName("nome-empresa")).Text;
+                item.DatePublication = element.FindElement(By.ClassName("publicado")).Text;
+
+                items.Add(item);
+            }
+
+            return Base.ConvertTo(items);
+        }
+    }
+}
---
+++ SiteEmpregos/Program.cs
@@ -0,0 +1,28 @@
+﻿using EasyAutomationFramework;
+using EasyAutomationFramework.Model;
+using OfficeOpenXml;
+using SiteEmpregos.Driver;
+using System.Data;
+
+WebScrapper web = new WebScrapper();
+DataTable vagas = web.GetData("https://www.empregos.com.br/vagas");
+
+using (var package = new ExcelPackage())
+{
+    var worksheet = package.Workbook.Worksheets.Add("VagasSheet");
+
+    worksheet.Cells.LoadFromDataTable(vagas, true);
+
+    string dirPath = @"C:\temp\excel";
+    string pathExcel = @"C:\temp\excel\Vagas.xlsx";
+
+    if (!Directory.Exists(dirPath))
+    {
+        Directory.CreateDirectory(dirPath);
+    }
+
+    var fileInfo = new System.IO.FileInfo(pathExcel);
+    package.SaveAs(fileInfo);
+}
+
+Console.WriteLine("Arquivo excel criado com sucesso!");
